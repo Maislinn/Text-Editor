fc1448ffe209b300f704e94e6a903e8d937bcd44
Maislinn <maislinn.helfer@gmail.com>
2023-04-08 10:57:32 -0700

    completing todos in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { //console.error('putDb not implemented');
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put(content);
+    await request;
+    console.log('putDb success');
+  }
+  catch (error) {
+    console.log('putDb error', error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { //console.error('getDb not implemented');
+  try {
+    const db = await initdb();
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const allContent = await store.getAll();
+    console.log('getDb success');
+    await tx.complete;
+    return allContent.content;
+  }
+  catch (error) {
+    console.log('getDb error', error);
+  }
+}
 
 initdb();
